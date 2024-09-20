0fe8ef3d7b169cc3001f7b1e42af9b4c45843973
vovka8101 <vova.sem143@gmail.com>
2024-09-20 14:59:43 +0300

    refactoring todo-list app

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-const newTodoBtn = document.querySelector(".btn-new-todo");
-const form = document.querySelector(".form");
-const todoCategory = document.getElementById("todo-categories");
-const todoName = document.getElementById("todo-input-name");
-const todoText = document.getElementById("todo-input-text");
-const addTodoBtn = document.getElementById("btn-add-todo");
-const todoList = document.querySelector(".todo-list");
+/** @type {HTMLButtonElement} */
+const newTodoBtn = document.getElementById("btn-toggle-add-todo");
 
-function addNewTodo() {
-  form.classList.toggle("hidden");
+/** @type {HTMLFormElement} */
+const addTodoForm = document.getElementById("form-add-todo");
 
-  if (form.classList.contains("hidden")) {
+const todoList = document.getElementById("todo-list");
+
+function toggleAddTodo() {
+  addTodoForm.classList.toggle("hidden");
+
+  if (addTodoForm.classList.contains("hidden")) {
     newTodoBtn.innerText = "Додати справу";
   } else {
     newTodoBtn.innerText = "Відмінна";
   }
 }
 
-newTodoBtn.addEventListener("click", addNewTodo);
-
-todoList.addEventListener("click", function(e) {
+function reduceTodoItem() {
   switch (e.target.dataset.action) {
     case 'remove':
       e.target.closest(".todo-item").remove();
@@ -32,32 +30,24 @@ todoList.addEventListener("click", function(e) {
     default:
       break;
   }
-});
+}
 
-addTodoBtn.addEventListener("click", function() {
-  if (!todoName.value || !todoText.value) return;
-  
-  console.log("add")
-  const todoItem = document.createElement("li");
-  todoItem.setAttribute("class", "todo-item");
+function addNewTodo(event) {
+  event.preventDefault();
 
-  todoItem.innerHTML = `
-    <div class="todo-item-head">
-      <div class="todo-category">${todoCategory.value}</div>
-      <div class="todo-head-buttons">
-        <button data-action="edit" class="btn-edit">edit</button>
-        <button data-action="remove" class="btn-remove">remove</button>
-      </div>
-    </div>
-    <div class="todo-item-body">
-      <h3 class="todo-item-name">${todoName.value}</h3>
-      <p class="todo-item-content">${todoText.value}</p>
-      <button data-action="complete" class="btn-complete primary-btn">Завершити</button>
-    </div>`;
-  
-  todoList.append(todoItem);
+  const todoData = {};
 
-  todoName.value = "";
-  todoText.value = "";
-  addNewTodo();
-});
+  Array.from(this.elements).forEach(element => {
+    if (element.name) {
+      todoData[element.name] = element.value;
+    }
+  });
+
+  console.log(todoData);
+}
+
+newTodoBtn.addEventListener("click", toggleAddTodo);
+
+todoList.addEventListener("click", reduceTodoItem);
+
+addTodoForm.addEventListener("submit", addNewTodo);
