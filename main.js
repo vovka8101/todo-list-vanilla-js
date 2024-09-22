3be5c024036a1a5a89e78e3066a00676a0d4af42
vovka8101 <vova.sem143@gmail.com>
2024-09-22 16:17:46 +0300

    fix styles

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const newTodoBtn = document.getElementById("btn-toggle-add-todo");
 
 /** @type {HTMLFormElement} */
 const addTodoForm = document.getElementById("form-add-todo");
+/** @type {HTMLFormElement} */
+const searchTodoForm = document.getElementById("form-search-todo");
 
 const todoList = document.getElementById("todo-list");
 
@@ -16,34 +18,62 @@ function toggleAddTodo() {
   }
 }
 
-function reduceTodoItem() {
-  switch (e.target.dataset.action) {
+/**
+ * @param {MouseEvent} e
+*/
+function reduceTodoItem(e) {
+  /** @type {HTMLButtonElement} */
+  const btnTarget = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+
+  switch (btnTarget.dataset.action) {
     case 'remove':
-      e.target.closest(".todo-item").remove();
+      btnTarget.closest(".todo-item").remove();
       break;
     case 'edit':
       console.log("edit");
       break;
     case 'complete':
-      e.target.closest(".todo-item").classList.toggle("completed");
+      btnTarget.closest(".todo-item").classList.toggle("completed");
       break;
     default:
       break;
   }
 }
 
-function addNewTodo(event) {
-  event.preventDefault();
+/**
+ * @param {Event} e
+ * @this {HTMLFormElement}
+*/
+function addNewTodo(e) {
+  e.preventDefault();
 
   const todoData = {};
 
-  Array.from(this.elements).forEach(element => {
-    if (element.name) {
-      todoData[element.name] = element.value;
+  Array.from(this.elements).forEach(el => {
+    if (el.name) {
+      todoData[el.name] = el.value;
     }
   });
+}
 
-  console.log(todoData);
+/**
+ * @param {Event} e
+ * @this {HTMLFormElement}
+*/
+function searchTodo(e) {
+  e.preventDefault();
+
+  const searchData = {
+    filters: []
+  };
+
+  Array.from(this.elements).forEach(el => {
+    if (el.name) {
+      searchData[el.name] = el.value;
+    } else if (el.type === 'checkbox' && el.checked) {
+      searchData.filters.push(el.value);
+    }
+  });
 }
 
 newTodoBtn.addEventListener("click", toggleAddTodo);
@@ -51,3 +81,5 @@ newTodoBtn.addEventListener("click", toggleAddTodo);
 todoList.addEventListener("click", reduceTodoItem);
 
 addTodoForm.addEventListener("submit", addNewTodo);
+
+searchTodoForm.addEventListener("submit", searchTodo);
